944135159d3d43b589e8ccf9b8d157ca3b3444d2
ALESSANDRODASILVALIMA <alessandrodasilva93@gmail.com>
2023-09-29 21:46:07 -0300

    Adicionando implementação do padrão Factory Method Uuma implementação do padrão Factory Method no  projeto. Isso permite a criação de documentos (PDF e CSV) de forma flexível e desacoplada, permitindo que diferentes tipos de documentos sejam adicionados sem afetar o código cliente.
    - Foi Introduzida  as seguintes  interface Document, com implementações concretas PDFDocument e CSVDocument.
    - Criada a hierarquia DocumentFactory, com interfaces e implementações concretas (PDFDocumentFactory e CSVDocumentFactory) para criar os documentos.
    - O DocumentGenerator para demonstrar a criação de documentos usando as fábricas correspondentes.
    
    Esta abordagem melhora a extensibilidade e a manutenção do nosso código, permitindo a adição fácil de novos tipos de documentos no futuro.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        public static void main(String[] args) throws IOException {
+        // Crie uma fábrica de PDF
+        DocumentFactory pdfFactory = new PDFDocumentFactory();
+        Document pdfDocument = pdfFactory.createDocument();
+        pdfDocument.createDocument();
+
+        // Crie uma fábrica de CSV
+        DocumentFactory csvFactory = new CSVDocumentFactory();
+        Document csvDocument = csvFactory.createDocument();
+        csvDocument.createDocument();
     }
 }
